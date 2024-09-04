6693a580b9fb0cb68f57a3334b0664289795f2b8
emesefedev <emesefedev@gmail.com>
2024-09-04 11:58:17 +0200

    game logic loop

--- game.js
+++ game.js
@@ -1,6 +1,6 @@
-function GameBoard() {
-    const rows = 3
-    const columns = 3
+function GameBoard(totalRows, totalColumns) {
+    const rows = totalRows
+    const columns = totalColumns
     const board = []
   
     const createBoard = () => {
@@ -25,6 +25,45 @@ function GameBoard() {
         }
         return true
     }
+
+    const isWinner = (player) => {
+        // Check rows
+        let r = 0
+        for (const row of board) {
+            if (row.every(cell => cell.getValue() === player.mark)) {
+                console.log(`${player.mark} wins for row ${r}`)
+                return true
+            }
+            r++
+        }
+
+        // Check columns
+        for (let j = 0; j < columns; j++) {
+            if (board[0][j].getValue() === player.mark 
+                && board[0][j].getValue() === board[1][j].getValue() 
+                && board[1][j].getValue() === borad[2][j].getValue()) {
+                    console.log(`${player.mark} wins for column ${j}`)
+                    return true
+                }
+        }
+
+        // Check diagonals
+        if (board[0][0].getValue() === player.mark 
+            && board[0][0].getValue() === board[1][1].getValue() 
+            && board[1][1].getValue() === borad[2][2].getValue()) {
+                console.log(`${player.mark} wins for diagonal 1`)
+                return true
+            }
+        
+        if (board[0][2].getValue() === player.mark 
+            && board[0][2].getValue() === board[1][1].getValue() 
+            && board[1][1].getValue() === borad[2][0].getValue()) {
+                console.log(`${player.mark} wins for diagonal 1`)
+                return true
+            }
+        
+        return false
+    }
   
     const addMarkToBoard = (row, column, player) => {
       board[row][column].addMark(player);
@@ -41,7 +80,7 @@ function GameBoard() {
     // Once we create the object, we create the board
     createBoard()
   
-    return { getBoard, addMarkToBoard, printBoard, isValidMarkPosition }
+    return { getBoard, addMarkToBoard, printBoard, isValidMarkPosition, isWinner }
 }
 
 function Cell() {
@@ -56,24 +95,24 @@ function Cell() {
     }
   
     return { addMark, getValue }
-  }
+}
+
+class Player {
+    constructor(name, mark) {
+        this.name = name
+        this.mark = mark
+    }
+}
 
 function GameController(
     playerOneName = "Player One",
     playerTwoName = "Player Two"
 ) {
-    const board = GameBoard()
+    const board = GameBoard(3, 3)
 
-    // TODO: Create a Player Object
     const players = [
-        {
-            name: playerOneName,
-            mark: "X"
-        },
-        {
-            name: playerTwoName,
-            mark: "O"
-        }
+        new Player(playerOneName, "X"),
+        new Player(playerTwoName, "O")
     ]
 
     let currentTurn = players[0]
@@ -94,29 +133,41 @@ function GameController(
     const playRound = (row, column) => {
         console.log(`Adding ${getCurrentTurn().name}'s mark into (${row}, ${column})...`)
 
-        console.log(`${board.isValidMarkPosition(row, column)}`)
-
         if (!board.isValidMarkPosition(row, column)) {
-            console.log(`Invalid position (${row}, ${column})... Try again`) 
+            console.log(`Invalid position (${row}, ${column})... Try again`)
+            row = prompt("row")
+            column = prompt("column")   
         }
         else {
             board.addMarkToBoard(row, column, getCurrentTurn())
         }
-
-        // TODO: Check if player has won
-
-        // If there's no winner...
-        changeTurn()
-        printNewRound()
     }
 
-    // Initial play game message
-    printNewRound()
+    const playGame = () => {
+        printNewRound()
 
+        let row = prompt("row")
+        let column = prompt("column")
+        
+        playRound(row, column)
+
+        while (!board.isWinner(getCurrentTurn())) {
+            changeTurn()
+            printNewRound()
+
+            row = prompt("row")
+            column = prompt("column")
+            playRound(row, column)   
+        }
+
+        console.log(`${getCurrentTurn().name} WINS!`)
+    }
+    
     return {
-        playRound,
+        playGame,
         getCurrentTurn
     }
 }
   
 const game = GameController()
+game.playGame()
